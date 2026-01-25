f9faf4dec70ae70d147aab989ff9af5be1bd9dae
AliSaleemHasan <ali1saleem1hasan@gmail.com>
2026-01-25 10:17:06 +0100

    Update C# Tree-sitter query for controllers and services
    Improved attribute parsing and added support for base classes to distinguish between Controllers and Services.

--- src/components/extractor/queries/controllers/c_sharp.scm
+++ src/components/extractor/queries/controllers/c_sharp.scm
@@ -1,22 +1,22 @@
-; C# Controller Query
+; C# Controller and Service Query
 (compilation_unit
-  (using_directive) @import_statement
-
   (namespace_declaration
     (declaration_list
       (class_declaration
         (attribute_list
           (attribute
-            name: (identifier) @attr_name
+            name: (identifier) @class_attr
             (attribute_argument_list
               (attribute_argument
                 (string_literal) @base_path
               )
             )?
-            (#eq? @attr_name "Route")
           )
-        )
+        )*
         name: (identifier) @class_name
+        (base_list
+             (identifier) @superclass
+        )?
         body: (declaration_list
           (method_declaration
             (attribute_list
@@ -28,7 +28,7 @@
                   )
                 )?
               )
-            )
+            )*
             name: (identifier) @method_name
           ) @method_definition
         )
