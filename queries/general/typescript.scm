642e71fcb5f07f77066b01c354e13bf5b6af939b
AliSaleemHasan <ali1saleem1hasan@gmail.com>
2026-02-15 15:46:24 +0100

    feat(tree-sitter): Update TypeScript query to capture method decorators

--- queries/general/typescript.scm
+++ queries/general/typescript.scm
@@ -1,56 +1,62 @@
-(
-  (export_statement
-    (decorator
-      (call_expression 
-        function: (identifier) @class_decorator_name
-        arguments: (arguments (string) @class_decorator_path)?
+(export_statement
+  (decorator
+    (call_expression 
+      function: (identifier) @class_decorator_name
+      arguments: (arguments (string) @class_decorator_path)?
+    )
+  )? @class_decorator
+  declaration: (class_declaration
+    name: (type_identifier) @class_name
+    [
+    (class_body
+      (method_definition
+        name: (property_identifier) @method_name
+      ) @method_definition
+    )
+    
+
+    (class_body
+      (decorator
+        (call_expression
+          function: (identifier) @decorator_name
+          arguments: (arguments (string) @decorator_path)?
+        )
+      ) @arrow_method_decorator
+      .
+      (public_field_definition
+        name: (property_identifier) @method_name
+        value: (arrow_function)
+      ) @method_definition
+    )
+
+	(class_body
+        (decorator
+          (call_expression
+            function: (identifier) @decorator_name
+            arguments: (arguments (string) @decorator_path)?
+          )
+        ) @method_decorator
+        .
+        (method_definition
+          name: (property_identifier) @method_name
+        ) @method_definition
       )
-    )? @class_decorator
-    declaration: (class_declaration
-      name: (type_identifier) @class_name
-    ) @class_node
-  )
+
+		(class_body
+          (public_field_definition
+            name: (property_identifier) @method_name
+            value: (arrow_function)
+          ) @method_definition
+    	)
+
+
+    ]
+    
+  ) @class_node
 )
 
-(class_body
-  (decorator
-    (call_expression
-      function: (identifier) @decorator_name
-      arguments: (arguments (string) @decorator_path)?
-    )
-  ) @method_decorator
-  .
-  (method_definition
-    name: (property_identifier) @method_name
-  ) @method_definition
-)
 
-(class_body
-  (method_definition
-    name: (property_identifier) @method_name
-  ) @method_definition
-)
 
-(class_body
-  (decorator
-    (call_expression
-      function: (identifier) @decorator_name
-      arguments: (arguments (string) @decorator_path)?
-    )
-  ) @arrow_method_decorator
-  .
-  (public_field_definition
-    name: (property_identifier) @method_name
-    value: (arrow_function)
-  ) @method_definition
-)
-
-(class_body
-  (public_field_definition
-    name: (property_identifier) @method_name
-    value: (arrow_function)
-  ) @method_definition
-)
 
 
 (lexical_declaration
